0e7baabd8f05f2db423f6bde826ef4c22f13607c
Matthew Brown <matthew@bargrove.com>
2018-01-14 23:34:59 +0000

    Add JUnit and Example Test File

---
+++ src/main/test/com/test/SabbTest.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SabbTest {
+
+  public SabbTest() {}
+
+  @Test
+  public void SabbTestExample() {
+    System.out.println("el testo");
+    Assert.assertEquals(1,1);
+  }
+}
