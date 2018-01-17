e6576f7dea1480093aecf47ed65dcca079e5e302
Cyrile Verdeyen <cyrile.verdeyen>
2018-01-17 14:32:35 +0000

    added some more logic and functionality to login, as well as eddit syntax.

--- src/main/resources/static/js/loginregister.js
+++ src/main/resources/static/js/loginregister.js
@@ -1,46 +1,63 @@
-$(document).ready(function()){
-  $("#REGISTER").click(function()){
+$(document).ready(function(){
+  $("#REGISTER").click(function(){
     var email = $("#registeremail");
     var password = $("#registerpassword");
     var passwordc = $("#registerpasswordc");
     if (password = passwordc){
-    var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "http://{{host}}:{{port}}/api/user",
-    "method": "POST",
-    "headers": {
-      "Content-Type": "application/json"
-    },
-    "processData": false,
-    "data": "{\n  \"emailAddress\": \email\,\n  \"password\": \password\\n}"
-    }
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://{{host}}:{{port}}/api/user",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "processData": false,
+        "data": "{\n  \"emailAddress\": \email\,\n  \"password\": \password\\n}"
+      }
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
   }
   else {
     window.alert("Your passwords do not match");
     formregister.reset();
   }
-  }
+});
 
-  $("#LOGIN").click(function()){
-    var email = $("#email");
-    var password = $("#password");
-    var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "http://{{host}}:{{port}}/api/user/{{user_emailaddress}}",
-    "method": "GET",
-    "headers": {}
+  $("#LOGIN").click(function(){
+      var email = $("#email");
+      var password = $("#password");
+      var responseemail = "";
+      var responsepassword = "";
+      var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://{{host}}:{{port}}/api/user/{{email}}",
+      "method": "GET",
+      "headers": {}
     }
 
     $.ajax(settings).done(function (response) {
       console.log(response);
-      var responseemail = response.value.emailAddress;
-
+      responseemail = response.value.emailAddress;
+      responsepassword = response.value.password;
     });
-  }
-}
+
+    if (password != responsepassword){
+      window.alert("Email or password aren't correct");
+      formlogin.reset();
+    }
+    else {
+      window.alert("Logged in!");
+      if (localStorage.useremail == email) {
+        console.log("there is a session open");
+      } else {
+        localStorage.useremail = email;
+        console.log("created local storage for email");
+      }
+
+    }
+  });
+});
