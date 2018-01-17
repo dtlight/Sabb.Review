5de40ea8e8700132ef8f37b406fd27a603eda75e
Cyrile Verdeyen <cyrile.verdeyen>
2018-01-17 13:48:19 +0000

    Registration js completed

--- src/main/resources/static/js/loginregister.js
+++ src/main/resources/static/js/loginregister.js
@@ -1,5 +1,28 @@
 $(document).ready(function()){
-  $("REGISTER").click(function()){
+  $("#REGISTER").click(function()){
+    var email = $("#registeremail");
+    var password = $("#registerpassword");
+    var passwordc = $("#registerpasswordc");
+    if (password = passwordc){
+    var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://{{host}}:{{port}}/api/user",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "processData": false,
+    "data": "{\n  \"emailAddress\": \email\,\n  \"password\": \password\\n}"
+    }
 
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  }
+  else {
+    window.alert("Your passwords do not match");
+    formregister.reset();
+  }
   }
 }
