70d60b7f0ac3044125744a596415a07cbd018e8f
Dave Light <davetlight@gmail.com>
2018-01-17 20:05:08 +0000

    creating new branch for Application controller

---
+++ src/main/java/com/sabbreview/controller/ApplicationController.java
@@ -0,0 +1,43 @@
+package com.sabbreview.controller;
+
+import com.sabbreview.SabbReview;
+import com.sabbreview.model.User;
+import com.sabbreview.responses.TransactionState;
+import com.sabbreview.responses.TransactionStatus;
+import com.sabbreview.responses.ValidationException;
+
+import javax.persistence.EntityManager;
+
+public class ApplicationController {
+
+    private static EntityManager em = SabbReview.getEntityManager();
+
+    public static TransactionState<User> registerApplication(User applicant) {
+
+        try{
+            em.getTransaction().begin();
+            if(!applicant.get().matches("")){
+                throw new ValidationException("empty field");
+            } else {
+                em.persist(applicant);
+            }
+        } catch (ValidationException e) {
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, e.getMessage());
+        } finally {
+            em.getTransaction().commit();
+        }
+
+        return new TransactionState<>(applicant, TransactionStatus.STATUS_OK);
+    }
+
+    public static TransactionState<User> getApplicant(String id) {
+        User applicant = em.find(User.class, id);
+        if (applicant == null) {
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "Could not find applicant");
+        } else {
+            return new TransactionState<>(applicant, TransactionStatus.STATUS_OK);
+        }
+    }
+
+
+}
