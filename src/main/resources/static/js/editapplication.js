fdc62bb8a6c4a85428b29a246516e76e8d6541b4
Alexander Clarke <alexander_m_clarke@live.co.uk>
2018-01-18 16:16:32 +0000

    Link html with js, wrote some js code.

--- src/main/resources/static/js/editapplication.js
+++ src/main/resources/static/js/editapplication.js
@@ -1,3 +1,48 @@
-$(document).ready(function()){
+$(document).ready(function(){
+    $("#submitForm").click( function(){
 
-}
+        alert("hiworld!");
+
+        //---SECTION 1 VARS---//
+        var name = $("#name");
+        var department = $("#department");
+        var appointmentDate = $("#appointmentDate");
+
+        //'Contact details during leave'
+        var presentPost = $("#presentPost");
+        var address = $("#address");
+        var email = $("#email");
+
+        //Emergency contact details
+        var emergencyName = $("#emergencyContactName");
+        var emergencyTel = $("#emergencyTelephoneNo");
+        var emergencyAddress  = $("#emergencyAddress");
+        var remuneration = $("#remuneration");
+
+        //---SECTION 2 VARS---//
+        var termsCount = $("#termsCount");
+        var lastSabbaticalDate  = $("#lastSabbaticalDate");
+        var lastSabbaticalReport = $("#lastSabbaticalReport");
+
+        //'Last three research outputs'
+        var outputOne = $("#researchOutput1");
+        var outputTwo = $("#researchOutput2");
+        var outputThree = $("#researchOutput3");
+
+        var sabbaticalJustification = $("#leaveJustification");
+        var sabbaticalStartDate = $("#leaveStartDate");
+        var sabbaticalEndDate  = $("#leaveEndDate");
+
+        var objectives = $("#leaveObjectives");
+        var collegeStrategyDesc = $("#collegeStrategicAlign");
+        var anticipatedOutputs = $("#anticipatedOutputs");
+
+        var sabbaticalprogram = $("#programmeOutline");
+        var grants = $("#appliedGrants");
+        var professionalDevelopmentDesc = $("#professionalDevelopmentOutline");
+
+        //TODO - Add signature box code.
+    });
+
+});
+
