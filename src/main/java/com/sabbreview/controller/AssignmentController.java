9d86363e94e92d80bdcefd86bfe2e242c8bd0ef4
Dave Light <davetlight@gmail.com>
2018-01-21 23:59:17 +0000

    Additional Code Added.

--- src/main/java/com/sabbreview/controller/AssignmentController.java
+++ src/main/java/com/sabbreview/controller/AssignmentController.java
@@ -3,7 +3,6 @@ package com.sabbreview.controller;
 import com.sabbreview.SabbReview;
 import com.sabbreview.model.Assignment;
 import com.sabbreview.model.User;
-import com.sabbreview.model.Application;
 import com.sabbreview.responses.TransactionState;
 import com.sabbreview.responses.TransactionStatus;
 
@@ -12,7 +11,7 @@ import javax.persistence.EntityManager;
 public class AssignmentController {
     private static EntityManager em = SabbReview.getEntityManager();
 
-    public static TransactionState<Assignment> setOwner(User owner, Application assignment) {
+    public static TransactionState<Assignment> setOwner(User owner, Assignment assignment) {
         try {
             em.getTransaction().begin();
             assignment.setOwner(owner);
@@ -23,4 +22,41 @@ public class AssignmentController {
             return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
         }
     }
+
+    public static TransactionState<Assignment> getOwner(String owner) {
+        Assignment assignment;
+        try {
+            em.getTransaction().begin();
+            assignment = em.find(Assignment.class, owner);
+            em.getTransaction().commit();
+            return new TransactionState<>(assignment, TransactionStatus.STATUS_OK, "");
+        } catch (Exception e) {
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
+        }
+    }
+
+    public static TransactionState<Assignment> setComments(String principle, Assignment comments) {
+        try {
+            if(comments == null) comments = new Assignment();
+            em.getTransaction().begin();
+            User user = em.find(User.class, principle);
+            comments.setComments(user);
+            em.persist(comments);
+            em.getTransaction().commit();
+            return new TransactionState<>(comments, TransactionStatus.STATUS_OK, "");
+        } catch (Exception e) {
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
+        }
+    }
+
+  //set acceptance state
+
+
+    //get comments
+
+
+
+    //get acceptance state
+
+
 }
