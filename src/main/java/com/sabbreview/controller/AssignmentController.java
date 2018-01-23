e3312efbc8635b00c6f057f57519b8c5657c5b52
Dave Light <davetlight@gmail.com>
2018-01-23 20:38:17 +0000

    resolved merge conflict in Assignment model, added 'deleteAssignment' and changeAssignment' to AssignmentController

--- src/main/java/com/sabbreview/controller/AssignmentController.java
+++ src/main/java/com/sabbreview/controller/AssignmentController.java
@@ -5,13 +5,27 @@ import com.sabbreview.model.Assignment;
 import com.sabbreview.model.User;
 import com.sabbreview.responses.TransactionState;
 import com.sabbreview.responses.TransactionStatus;
+import com.sabbreview.responses.ValidationException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.RollbackException;
 
 public class AssignmentController {
     private static EntityManager em = SabbReview.getEntityManager();
 
-    public static TransactionState<Assignment> setOwner(User owner, Assignment assignment) {
+
+    public static TransactionState<Assignment> createApplication(String principle, Assignment assignment) {
+        try {
+            em.getTransaction().begin();
+            em.persist(assignment); //telling jpa to store
+            em.getTransaction().commit();
+        } catch (RollbackException e) {
+
+        } catch (ValidationException e) {
+
+        }
+    }
+    /*public static TransactionState<Assignment> setOwner(User owner, Assignment assignment) {
         try {
             em.getTransaction().begin();
             assignment.setOwner(owner);
@@ -23,19 +37,17 @@ public class AssignmentController {
         }
     }
 
-    public static TransactionState<Assignment> getOwner(String owner) {
-        Assignment assignment;
+    public static TransactionState<Assignment> getOwner(String owner) { /////     Assignment assignment;
         try {
-            em.getTransaction().begin();
-            assignment = em.find(Assignment.class, owner);
-            em.getTransaction().commit();
+            Assignment assignment = em.find(Assignment.id, owner);
             return new TransactionState<>(assignment, TransactionStatus.STATUS_OK, "");
         } catch (Exception e) {
             return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
         }
     }
 
-    public static TransactionState<Assignment> setComments(String principle, Assignment comments) {
+    public static TransactionState<Assignment> setComments(String principle, Assignment comments) { ////////// em.find to find the assignment, then getComments and then add, then re-merge
+
         try {
             if(comments == null) comments = new Assignment();
             em.getTransaction().begin();
@@ -47,7 +59,7 @@ public class AssignmentController {
         } catch (Exception e) {
             return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
         }
-    }
+    }*/
 
   //set acceptance state
 
