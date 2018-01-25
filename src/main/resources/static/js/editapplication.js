d01fd2875c711e5b3298de19c67d074fbf5d387a
Alexander Clarke <alexander_m_clarke@live.co.uk>
2018-01-25 10:23:28 +0000

    Create editapplication CSS, add element names to JS

--- src/main/resources/static/js/editapplication.js
+++ src/main/resources/static/js/editapplication.js
@@ -1,8 +1,51 @@
 $(document).ready(function(){
     $(function() {
-        console.log("loaded editapplication header+footer");
         $('#navbar').load('navbar.html');
         $('#footer').load('footer.html');
+
+        //Need to load existing fields in here.
+    });
+
+    $("#submitForm").submit(function (e) {
+        /*$('#name');
+        $('#department');
+        $('#address');
+
+        $('#appointmentDate');
+        $('#presentPost');
+        $('#email');
+
+        $('#emergencyContactName');
+        $('#emergencyTelephoneNo');
+        $('#emergencyEmail');
+
+        $('#emergencyAddress');
+        $('#remuneration');
+        $('#termsCount');
+
+        $('#lastSabbaticalDate');
+        $('#sabbaticalObjectives');
+        $('#lastSabbaticalReport');
+
+        $('#researchOutput1');
+        $('#researchOutput2');
+        $('#researchOutput3');
+
+        $('#leaveStartDate');
+        $('#leaveEndDate');
+        $('#leaveJustification');
+
+        $('#leaveObjectives');
+        $('#collegeStrategicAlign');
+        $('#anticipatedOutputs');
+        $('#programmeOutline');
+        $('#appliedGrants');
+        $('#professionalDevelopmentOutline');
+
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });*/
     });
 
 });
