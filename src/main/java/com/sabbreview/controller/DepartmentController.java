d2fa11a21985ac92bb6f7602e0ff7550cc1a29f7
Dave Light <davetlight@gmail.com>
2018-01-29 14:51:25 +0000

    Fill in createDepartment And deleteDepartment

--- src/main/java/com/sabbreview/controller/DepartmentController.java
+++ src/main/java/com/sabbreview/controller/DepartmentController.java
@@ -1,5 +1,55 @@
 package com.sabbreview.controller;
 
+import com.sabbreview.model.Department;
+import com.sabbreview.model.User;
+import com.sabbreview.responses.AuthenticationException;
+import com.sabbreview.responses.TransactionState;
+import com.sabbreview.responses.TransactionStatus;
+
+import static spark.Spark.delete;
+import static spark.Spark.post;
+
 public class DepartmentController extends Controller {
 
+    private static TransactionState<Department> createDepartment(String principle, Department department){
+        try {
+            if (department == null) {
+                department = new Department();
+            }
+            em.getTransaction().begin();
+            User HOD = em.find(User.class, principle);
+            department.setHOD(HOD);
+            em.persist(department);
+            em.getTransaction().commit();
+            return new TransactionState<>(department, TransactionStatus.STATUS_OK, "");
+        } catch (Exception e) {
+            rollback();
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
+        }
+    }
+
+    private static TransactionState<Department> deleteDepartment(String principle, String depID) {
+        try {
+            em.getTransaction().begin();
+            Department department = em.find(Department.class, depID);
+            if (department.getHOD().getDepartment().equals(principle)) {
+                em.remove(department);
+            } else {
+                throw new AuthenticationException();
+            }
+            em.getTransaction().commit();
+            return new TransactionState<>(null, TransactionStatus.STATUS_OK, "");
+        } catch (Exception e) {
+            rollback();
+            return new TransactionState<>(null, TransactionStatus.STATUS_ERROR, "");
+        }
+    }
+
+    public static void attach() {
+        delete("/department/depID", (req, res) -> requireAuthentication(req,
+                (principle) -> toJson(DepartmentController.deleteDepartment(principle, req.params("depID")))));
+
+        post("/department", (req, res) -> requireAuthentication(req, (principle) -> toJson(
+                DepartmentController.createDepartment(principle, fromJson(req.body(), Department.class)))));
+    }
 }
--- src/main/java/com/sabbreview/model/Department.java
+++ src/main/java/com/sabbreview/model/Department.java
@@ -1,8 +1,15 @@
 package com.sabbreview.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name = "department") public class Department extends Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String id;
 
     private User HOD;
 
@@ -13,6 +20,15 @@ import javax.persistence.Entity;
     public Department setHOD(User HOD) {
         this.HOD = HOD;
         return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+
     }
 
 }
