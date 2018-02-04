8610eebb5b80a5dd65044fe83bd3955e643ce920
coo_coo1 <kaloianbch@gmail.com>
2018-02-04 17:50:09 +0000

    Add mailgun API to pom.xml, update MailController class, update gitignore, add EmailTest class

---
+++ src/main/java/com/sabbreview/test/EmailTest.java
@@ -0,0 +1,9 @@
+package com.sabbreview.test;
+
+import com.sabbreview.controller.EmailController;
+
+public class EmailTest {
+    public static void main (String []args){
+        EmailController.Send("HELLO","\uD83D\uDE4B\u200D","kaloianbch@gmail.com");
+    }
+}
