ac6d79e21379b666f442852ec144191fbd5aa42b
Dave Light <davetlight@gmail.com>
2018-02-28 17:08:11 +0000

    Add Test Class For DeparmentController

---
+++ util/src/main/test/java/com/sabbreview/controller/DepartmentControllerTest.java
@@ -0,0 +1,42 @@
+package com.sabbreview.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartmentControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createDepartment() {
+    }
+
+    @Test
+    public void updateDepartment() {
+    }
+
+    @Test
+    public void getDepartment() {
+    }
+
+    @Test
+    public void getDepartments() {
+    }
+
+    @Test
+    public void deleteDepartment() {
+    }
+
+    @Test
+    public void getApplications() {
+    }
+}
