f4fc264b9d679f6c396199a937a5db322e1ca5e2
Dave Light <davetlight@gmail.com>
2018-03-03 18:19:45 +0000

    Add Test Class For Role Controllerer

---
+++ util/src/main/test/java/com/sabbreview/controller/RoleControllerTest.java
@@ -0,0 +1,26 @@
+package com.sabbreview.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoleControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createRole() {
+    }
+
+    @Test
+    public void removeRole() {
+    }
+}
