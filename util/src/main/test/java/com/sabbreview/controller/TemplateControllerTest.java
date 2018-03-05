4dbfeeb4d6f0d83173fe8de7beda82cfbbfab53b
Dave Light <davetlight@gmail.com>
2018-03-05 18:24:33 +0000

    Add Test Class For Template Controller

---
+++ util/src/main/test/java/com/sabbreview/controller/TemplateControllerTest.java
@@ -0,0 +1,38 @@
+package com.sabbreview.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemplateControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createTemplate() {
+    }
+
+    @Test
+    public void deleteTemplate() {
+    }
+
+    @Test
+    public void deleteTemplateField() {
+    }
+
+    @Test
+    public void getTemplate() {
+    }
+
+    @Test
+    public void addField() {
+    }
+}
