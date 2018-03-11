1b6beaf87d1a42a20ba75f4ccaeee864e7001411
Matthew Brown <matthew@bargrove.com>
2018-03-11 11:41:14 +0000

    Add Department Adapter

--- client/src/views/admin/department/index.js
+++ client/src/views/admin/department/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Row, Col, ListGroupItem, ListGroup} from 'reactstrap';
-import {FieldList, NewQuestion} from '../../../components/template/index.js';
 import {ApplicationList} from '../../../components/home/index.js';
 import {TemplateTable, CreateTemplate} from '../../../components/template/index.js';
 
@@ -23,11 +22,7 @@ export class DepartmentInfo extends React.Component {
           hod: data.value.HOD,
           name: data.value.name,
           applications: data.value.applications,
-<<<<<<< Updated upstream
-            templates: data.value.templateList
-=======
             templates: data.value.templates
->>>>>>> Stashed changes
         })
       }
     });
@@ -44,11 +39,7 @@ export class DepartmentInfo extends React.Component {
           <Row>
             <Col md={3} bg={'primary'} style={{"marginBottom": "1em"}}>
               <ListGroup>
-<<<<<<< Updated upstream
-                <ListGroupItem><strong>Head of Department</strong>: <br/>{(this.state.hod)?this.state.hod.emailAddress:""}</ListGroupItem>
-=======
                 <ListGroupItem><strong>Head of Department</strong>: <br/>{this.state.hod}</ListGroupItem>
->>>>>>> Stashed changes
               </ListGroup>
             </Col>
             <Col md={9}>
