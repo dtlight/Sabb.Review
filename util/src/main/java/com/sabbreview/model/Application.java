936b2c972a72718ed0877a2b2ef48cc44dd7408c
Dave Light <davetlight@gmail.com>
2018-03-12 15:17:17 +0000

    Add signature Field, Getters and Setters

--- util/src/main/java/com/sabbreview/model/Application.java
+++ util/src/main/java/com/sabbreview/model/Application.java
@@ -2,16 +2,7 @@ package com.sabbreview.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @NamedQueries({
     @NamedQuery(name="authenticated-delete", query = "delete from applications a where a.id = :id"),
@@ -39,6 +30,18 @@ public class Application
 
   @Enumerated()
   private AcceptanceState state;
+
+  @Lob
+  private String signature;
+
+  public void setSignature(String sign) {
+    this.signature = sign;
+
+  }
+
+  public String getSignature() {
+    return signature;
+  }
 
   public Application() {
 
