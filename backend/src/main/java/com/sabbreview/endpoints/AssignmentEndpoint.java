2bb5cca1c84d77de6e3590c0ae657566a23e61ad
Alexander Clarke <alexander_m_clarke@live.co.uk>
2018-03-12 11:35:48 +0000

    Undo test permissions structure
    I experimented with separate endpoints for reviewers and reviewees.
    Decided this wasn't the best solution, and I'm undoing it.

--- backend/src/main/java/com/sabbreview/endpoints/AssignmentEndpoint.java
+++ backend/src/main/java/com/sabbreview/endpoints/AssignmentEndpoint.java
@@ -1,6 +1,5 @@
 package com.sabbreview.endpoints;
 
-import com.sabbreview.controller.AssignmentController;
 import com.sabbreview.controller.UserController;
 import com.sabbreview.model.AcceptanceState;
 
@@ -16,15 +15,11 @@ public class AssignmentEndpoint extends Endpoint {
   public static void attach() {
     delete("/assignment/:id", (req, res) -> requireAuthentication(req,
         (principle) -> toJson(deleteAssignment(principle, req.params(":id")))));
-
     post("/assignment", (req, res) -> requireAuthentication(req,
         (principle -> toJson(createAssignment(principle, fromJson(req.body(),
             com.sabbreview.model.Assignment.class))))));
-
-    put("/application/:id/state/:state", (req, res) -> requireAuthentication(req,
-            (principle) -> toJson(
-                    AssignmentController.setAcceptanceState(principle, req.params(":id"), fromJson(req.params(":state"), AcceptanceState.class)))));
-
+    put("/assignment/:id/state/:state", (req, res) -> toJson(setAcceptanceState(req.params(":id"),
+        AcceptanceState.valueOf(req.params(":state")))));
     get("/user/assignments", (req, res) -> requireAuthentication(req,
         (principle -> toJson(UserController.getAssignmentsForUser(principle)))));
   }
