c3c5910c8c903ad9adc8f1ab652dffd9e08ca9ce
coo_coo1 <kaloianbch@gmail.com>
2018-03-14 16:44:15 +0000

    Add query for searching assignments by due date

--- util/src/main/java/com/sabbreview/DueDateWorker.java
+++ util/src/main/java/com/sabbreview/DueDateWorker.java
@@ -1,10 +1,12 @@
-package com.sabbreview;
+import com.sabbreview.model.Assignment;
 
 import java.time.LocalDate;
 
-public class DueDateWorker implements Runnable{
+public static class DueDateWorker implements Runnable extends Controller{
 
-    public void run() {
-        System.out.println(LocalDate.now());
+    public void run{
+            System.out.println(LocalDate.now());
+            em.getTransaction().begin();
+            Assignment assignment = em.find(Assignment.class, assignmentID);
     }
 }
