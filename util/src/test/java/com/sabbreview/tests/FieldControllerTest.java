67105c6fac2e826dbc1757faa798bf8761f0bd61
Dave Light <davetlight@gmail.com>
2018-03-14 16:05:22 +0000

    Edit Field Controller Test

--- util/src/test/java/com/sabbreview/tests/FieldControllerTest.java
+++ util/src/test/java/com/sabbreview/tests/FieldControllerTest.java
@@ -1,35 +1,73 @@
-package com.sabbreview.controller;
+package com.sabbreview.tests;
 
+import com.sabbreview.SabbReviewEntityManager;
+import com.sabbreview.controller.ApplicationController;
+import com.sabbreview.controller.FieldController;
+import com.sabbreview.controller.UserController;
+import com.sabbreview.model.Application;
+import com.sabbreview.model.Field;
+import com.sabbreview.model.User;
+import com.sabbreview.responses.TransactionState;
+import com.sabbreview.responses.TransactionStatus;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
 public class FieldControllerTest {
+    private Field testField;
+    private Application testApplication;
+    private User testuser = new User("test@test.sabb.review", "password");
 
     @Before
-    public void setUp() throws Exception {
+    public void setupAssignment() {
+        testApplication = new Application();
+        if((testuser = SabbReviewEntityManager.getEntityManager().find(User.class, testuser.getEmailAddress())) == null) {
+            testuser = new User("test@test.sabb.review", "password");
+            UserController.registerUser(testuser);
+        }
+        testApplication.setApplicant(testuser);
+        ApplicationController.createApplication(testuser.getEmailAddress(), testApplication);
     }
+
 
     @After
     public void tearDown() throws Exception {
     }
 
     @Test
-    public void createField() {
+    /**
+     * To to create and get a field.
+     */
+    public void getField() {
+        testField = new Field();
+        TransactionState<Field> fieldTransactionState = FieldController.createField(testuser.getEmailAddress(), testField);
+        Assert.assertEquals(TransactionStatus.STATUS_OK, fieldTransactionState.getState());
+        Assert.assertNotNull(fieldTransactionState.getValue());
+        Field field = fieldTransactionState.getValue();
+        Assert.assertNotNull(field.getId());
     }
 
     @Test
     public void editField() {
+
     }
 
-    @Test
-    public void getField() {
-    }
-
+    /**
+     * To to create and delete a field.
+     */
     @Test
     public void deleteField() {
+        testField = new Field();
+        TransactionState<Field> fieldTransactionState1 = FieldController.createField(testuser.getEmailAddress(), testField);
+        Assert.assertEquals(TransactionStatus.STATUS_OK, fieldTransactionState1.getState());
+        Assert.assertNotNull(fieldTransactionState1.getValue());
+        TransactionState<Field> fieldTransactionState = FieldController.deleteField(testuser.getEmailAddress(), testField.getId());
+        Assert.assertEquals(TransactionStatus.STATUS_OK, fieldTransactionState.getState());
+        Assert.assertNull(fieldTransactionState.getValue());
     }
 
     @Test
