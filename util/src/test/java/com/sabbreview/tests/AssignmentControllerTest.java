8f48a38a035c728d9a53eb478019c83ee0960460
Dave Light <davetlight@gmail.com>
2018-03-14 16:04:35 +0000

    Edit Assignment Controller Test

--- util/src/test/java/com/sabbreview/tests/AssignmentControllerTest.java
+++ util/src/test/java/com/sabbreview/tests/AssignmentControllerTest.java
@@ -1,6 +1,9 @@
-package com.sabbreview.;
+package com.sabbreview.tests;
 
 import com.sabbreview.SabbReviewEntityManager;
+import com.sabbreview.controller.ApplicationController;
+import com.sabbreview.controller.AssignmentController;
+import com.sabbreview.controller.UserController;
 import com.sabbreview.model.AcceptanceState;
 import com.sabbreview.model.Application;
 import com.sabbreview.model.Assignment;
@@ -12,36 +15,51 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class AssignmentControllerTest {
-    private Application testApplication
+    private Application testApplication;
     private User testuser = new User("test@test.sabb.review", "password");
 
-    {
-
-    }
-
     @Before
-    public void setupApplication() {
+    public void setupAssignment() {
         testApplication = new Application();
-        UserController.registerUser(testuser);
+        if((testuser = SabbReviewEntityManager.getEntityManager().find(User.class, testuser.getEmailAddress())) == null) {
+            testuser = new User("test@test.sabb.review", "password");
+            UserController.registerUser(testuser);
+        }
         testApplication.setApplicant(testuser);
         ApplicationController.createApplication(testuser.getEmailAddress(), testApplication);
-
     }
 
+    @Test
+    public void createAssignment() {
+        TransactionState<Assignment>  assignmentTransactionState = AssignmentController.createAssignment(testuser.getEmailAddress(), testApplication.getId(), testuser.getEmailAddress());
+        Assert.assertEquals(TransactionStatus.STATUS_OK, assignmentTransactionState.getState());
+        Assert.assertNotNull(assignmentTransactionState.getValue());
+        Assignment assignment = assignmentTransactionState.getValue();
+        Assert.assertNotNull(assignment.getId());
+        AssignmentController.deleteAssignment(testuser.getEmailAddress(), assignment.getId());
+    }
 
     @After
-    public void teardownApplication() {
+    public void teardownAssignment() {
         UserController.deleteUser(testuser.getEmailAddress());
         ApplicationController.deleteApplication(testuser.getEmailAddress(), testApplication.getId());
-
     }
 
     @Test
     public void setAcceptanceState() {
-        Application applicationFromDB = SabbReviewEntityManager.getEntityManager().find(Application.class, testApplication.getId());
-        Assert.assertEquals(AcceptanceState.PENDING, applicationFromDB);
+        TransactionState<Assignment>  assignmentTransactionState = AssignmentController.createAssignment(testuser.getEmailAddress(), testApplication.getId(), testuser.getEmailAddress());
+        Assignment assignment = assignmentTransactionState.getValue();
+        assignment.setState(AcceptanceState.COMPLETED);
+        Assert.assertEquals(AcceptanceState.COMPLETED, assignment.getState());
+        AssignmentController.deleteAssignment(testuser.getEmailAddress(), assignment.getId());
+    }
+
+    @Test
+    public void setAssignee() {
+        TransactionState<Assignment>  assignmentTransactionState = AssignmentController.createAssignment(testuser.getEmailAddress(), testApplication.getId(), testuser.getEmailAddress());
+        Assignment assignment = assignmentTransactionState.getValue();
+        Assert.assertEquals(testuser.getEmailAddress(), assignment.getAssignee().getEmailAddress());
+        AssignmentController.deleteAssignment(testuser.getEmailAddress(), assignment.getId());
     }
 }
