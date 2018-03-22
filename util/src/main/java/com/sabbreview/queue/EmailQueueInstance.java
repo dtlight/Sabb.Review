8cec4c0741b19256b1d86f80535e16d676eb5b81
coo_coo1 <kaloianbch@gmail.com>
2018-03-22 20:54:32 +0000

    Refactor Queue Package and Add JavaDocs.

--- util/src/main/java/com/sabbreview/queue/EmailQueueInstance.java
+++ util/src/main/java/com/sabbreview/queue/EmailQueueInstance.java
@@ -5,12 +5,15 @@ import com.rabbitmq.client.Consumer;
 
 import java.io.IOException;
 
+/**
+ * Manages channel and publishing messages to the CloudAMQP server.
+ */
 public class EmailQueueInstance {
   private static final String queueName = "email";
   private static Channel channel;
   public EmailQueueInstance() throws Exception {
     if(channel == null) {
-      channel = com.sabbreview.Queue.getConnection().createChannel();
+      channel = QueueManager.getConnection().createChannel();
       channel.queueDeclare(queueName, true, false, false, null);
     }
   }
