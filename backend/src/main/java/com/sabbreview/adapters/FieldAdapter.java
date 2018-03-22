2f441a4af0a4aa99fba83613d7cee5827c2b84af
Matthew Brown <matthew@bargrove.com>
2018-03-22 22:00:48 +0000

    Hotfix: CreatedAt

--- backend/src/main/java/com/sabbreview/adapters/FieldAdapter.java
+++ backend/src/main/java/com/sabbreview/adapters/FieldAdapter.java
@@ -17,7 +17,7 @@ public class FieldAdapter  implements JsonSerializer<Field> {
     object.addProperty("title", src.getTitle());
     object.addProperty("showAtEnd", src.isShowAtEnd());
     object.addProperty("type", src.getType().name());
-    object.addProperty("createdAt", src.getCreatedAt().toString());
+    object.addProperty("createdAt", (src.getCreatedAt()!=null)?src.getCreatedAt().toString():"");
 
     object.add("fieldOptions", context.serialize(src.getFieldOptions()));
 
